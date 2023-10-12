5357fd523f14e664ad146e17d93abcfb9c20dcbf
Naveen Kumar <naveen.kumar122@ad.infosys.com>
2023-10-12 09:25:56 +0530

    some modification

--- src/main/java/com/blog/config/securityConfig.java
+++ src/main/java/com/blog/config/securityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 import com.blog.security.JwtAuthenticationEntryPoint;
 import com.blog.security.JwtAuthenticationFilter;
